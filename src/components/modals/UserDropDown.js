346313def4aedfcb14e186ccd080a339aed3a5b0
Ness Hm <nessmahamidou92@gmail.com>
2024-12-09 22:19:04 +0100

    solve build errors

--- src/components/modals/UserDropDown.js
+++ src/components/modals/UserDropDown.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Link } from 'react-router-dom';
+import {AuthContext} from "../../context/context.ts";
 function UserDropDown() {
+    const { user: loggedInUser, logout } = useContext(AuthContext);
 
     return (
         <div className={"user-dropdown modaal"}>
