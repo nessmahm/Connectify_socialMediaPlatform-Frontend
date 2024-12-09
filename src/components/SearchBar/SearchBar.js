7c9d6d3f74fd4aec05e3fb878bd23caf52edb109
Ness Hm <nessmahamidou92@gmail.com>
2024-12-10 00:33:17 +0100

    add xss vul

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,25 +1,40 @@
 import React from 'react'
-import {findUserByName} from "./findUserByName";
-import {useNavigate} from "react-router-dom";
+import { findUserByName } from "./findUserByName";
+import { useNavigate } from "react-router-dom";
 
-function SearchBar({inputHandler, token ,searchText}) {
+function SearchBar({ inputHandler, token, searchText }) {
     const navigate = useNavigate();
-    const handleSearchClick = async () => {
+
+    const handleSearchClick = async (text) => {
         const userId = await findUserByName(searchText, token);
         navigate(`/profil/${userId}`);
-    }
+    };
+
+    // Introducing XSS by rendering raw user input into DOM directly.
+    const dangerouslySetHTML = (text) => {
+        // Dangerous and vulnerable to XSS
+        document.body.innerHTML = text;
+        handleSearchClick(text);
+    };
+
     return (
-      <div className="search-container">
-          <input type="text" name="search" placeholder="Search..."   onChange={inputHandler}
-                  className="search-input"  autoComplete="off" />
-          <a className="search-btn">
-              <i
-                  className="fas fa-search"
-                  onClick={handleSearchClick}
-              ></i>
-          </a>
-      </div>
-)
+        <div className="search-container">
+            <input
+                type="text"
+                name="search"
+                placeholder="Search..."
+                onChange={inputHandler}
+                className="search-input"
+                autoComplete="off"
+            />
+            <a
+                className="search-btn"
+                onClick={() => dangerouslySetHTML(searchText)} // Vulnerable to XSS
+            >
+                <i className="fas fa-search"></i>
+            </a>
+        </div>
+    );
 }
 
-export default SearchBar
+export default SearchBar;
